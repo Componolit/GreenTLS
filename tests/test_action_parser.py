68682ca41d6a1fa36f5e69c581e7798b4d1289e1
Abhinav Mishra <abhinav.drs@gmail.com>
2019-04-02 15:28:32 +0200

    Add ActionParser

--- tests/test_action_parser.py
+++ tests/test_action_parser.py
@@ -1,6 +1,9 @@
 import unittest
 
-from tools.action_parser import Action, ActionParser
+from rflx.expression import Number
+
+from tools.action_parser import (Action, ActionParser, Assignment, BooleanLiteral, Function,
+                                 Identifier, Literaldef, String, Variable)
 
 
 class TestActionParser(unittest.TestCase):  # pylint: disable=too-many-public-methods
@@ -13,45 +16,57 @@ class TestActionParser(unittest.TestCase):  # pylint: disable=too-many-public-me
 
     def test_assignement_literal(self) -> None:
         self.assert_action('server_certificate_type := X509',
-                           Action())
+                           Assignment(Identifier("server_certificate_type"), Literaldef("X509")))
 
     def test_assignment_boolean(self) -> None:
         self.assert_action('server_name_received := False',
-                           Action())
+                           Assignment(Identifier("server_name_received"), BooleanLiteral("False")))
 
     def test_assignement_numeral(self) -> None:
         self.assert_action('legacy_version := 772',
-                           Action())
+                           Assignment(Identifier("legacy_version"), Number(772)))
 
     def test_assignement_string(self) -> None:
-        self.assert_action('label := "c e traffic"',
-                           Action())
+        self.assert_action(
+            'label := "c e traffic"',
+            Assignment(Identifier("label"), String("c e traffic")))
 
     def test_assignement_numeral_hex(self) -> None:
         self.assert_action('legacy_version := 16#0304#',
-                           Action())
+                           Assignment(Identifier("legacy_version"), Number(772)))
 
     def test_assignement_numeral_bin(self) -> None:
         self.assert_action('legacy_version := 2#1100000100#',
-                           Action())
+                           Assignment(Identifier("legacy_version"), Number(772)))
 
     def test_assignment_function(self) -> None:
         self.assert_action('connection := read(application_in)',
-                           Action())
+                           Assignment(Identifier("connection"),
+                                      Function('read', [Variable('application_in')])))
 
     def test_assignment_function_no_arg(self) -> None:
         self.assert_action('connection := null()',
-                           Action())
+                           Assignment(Identifier("connection"), Function("null", [])))
 
     def test_assignment_function_multiple_args(self) -> None:
-        self.assert_action('derived_early_secret := Derive_Secret(early_secret, "derived", "")',
-                           Action())
+        self.assert_action('derived_early_secret := Derive_Secret(early_secret,"derived","")',
+                           Assignment(Identifier("derived_early_secret"),
+                                      Function('Derive_Secret',
+                                               [Variable('early_secret'),
+                                                String("derived"), String("")])))
 
     def test_assignment_nested_functions(self) -> None:
         self.assert_action('extensions_list := list(supported_versions_extension(list(772))))',
-                           Action())
+                           Assignment(Identifier("extensions_list"),
+                                      Function("list",
+                                               [Function('supported_versions_extension',
+                                                         [Function('list', [Number(772)])])])))
 
     def test_assignment_nested_functions_multiple_args(self) -> None:
         self.assert_action('extensions_list := append(extensions_list, '
                            'supported_groups_extension(configuration))',
-                           Action())
+                           Assignment(Identifier('extensions_list'),
+                                      Function('append',
+                                               [Variable('extensions_list'),
+                                                Function('supported_groups_extension',
+                                                         [Variable('configuration')])])))
--- tools/action_parser.py
+++ tools/action_parser.py
@@ -1,16 +1,116 @@
-from pyparsing import StringEnd, Word, alphas
+import typing as tp
+
+from pyparsing import (Forward, Keyword, Literal, Optional, ParseResults, Suppress, Word, alphanums,
+                       alphas, dblQuotedString, delimitedList, sglQuotedString)
+from rflx.expression import Number
+from rflx.parser import Parser
 
 
 class Action:
+    def __repr__(self) -> str:
+        args = '\n\t' + ',\n\t'.join(f"{k}={v!r}" for k, v in self.__dict__.items())
+        return f'{self.__class__.__name__}({args})'.replace('\t', '\t    ')
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return NotImplemented
+
+
+class Identifier(Action):
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+
+class Assignment(Action):
+    def __init__(self, left: Identifier, right: Action) -> None:
+        self.left = left
+        self.right = right
+
+
+class String(Action):
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+
+class Variable(Action):
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+
+class Literaldef(Action):
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+
+class BooleanLiteral(Literaldef):
     pass
+
+
+class Function(Action):
+    def __init__(self, name: str, args: tp.List[tp.Union[Number, Action]]) -> None:
+        self.name = name
+        self.args = args
 
 
 class ActionParser:
     def __init__(self) -> None:
-        action = Word(alphas)
-        action.setParseAction(Action)
+        identifier = Word(alphanums + "_")
+        identifier.setParseAction(parse_identifier)
+        literal = Word(alphanums + "_")
+        literal.setParseAction(parse_literal)
 
-        self.grammar = action + StringEnd()
+        boolean_literal = Keyword("True") | Keyword("False")
+        boolean_literal.setParseAction(parse_literal)
+
+        string = sglQuotedString | dblQuotedString
+        string.setParseAction(parse_string)
+
+        lpr = Suppress(Literal("("))
+        rpr = Suppress(Literal(")"))
+        functor_name = Word(alphanums + "_")
+
+        variable = Word(alphas + "_", exact=1) + Optional(Word(alphanums + "_"))
+        variable.setParseAction(parse_variable)
+
+        multi_arg_func: Forward = Forward()
+        arg = multi_arg_func | Parser.numeric_literal() | variable | string
+        args = delimitedList(arg)
+        multi_arg_func <<= functor_name + lpr + Optional(args) + rpr
+
+        multi_arg_func.setParseAction(parse_func)
+
+        assignment = (identifier + Keyword(":=")
+                      + (boolean_literal | string | Parser.numeric_literal()
+                         | multi_arg_func | literal))
+        assignment.setParseAction(parse_assignment)
+
+        self.grammar = assignment
 
     def parse(self, string: str) -> Action:
-        return self.grammar.parseString(string)
+        return self.grammar.parseString(string)[0]
+
+
+def parse_assignment(tokens: ParseResults) -> Assignment:
+    return Assignment(tokens[0], tokens[2])
+
+
+def parse_identifier(tokens: ParseResults) -> Identifier:
+    return Identifier(tokens[0])
+
+
+def parse_literal(tokens: ParseResults) -> Literaldef:
+    return Literaldef(tokens[0])
+
+
+def parse_string(tokens: ParseResults) -> String:
+    return String(tokens[0][1:-1])
+
+
+def parse_func(tokens: ParseResults) -> Function:
+    return Function(tokens[0], tokens[1:])
+
+
+def parse_variable(tokens: ParseResults) -> Variable:
+    tokens[0] = "".join(tokens)
+    return Variable(tokens[0])
